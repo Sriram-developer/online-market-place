83768db839adbbe4bf18d06d4bad7f3826ccd88d
Sriram <sri761177@gmail.com>
2023-10-31 19:27:25 +0530

    Add Link And updated the prisma schema

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,9 @@
 import { SignInButton, UserButton, useUser } from "@clerk/nextjs";
+import Link from "next/link";
+
 export function NavBar() {
+    const user = useUser();
+
     return(
 <nav className="bg-white border-gray-200 dark:bg-gray-900">
   <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
@@ -15,16 +19,28 @@ export function NavBar() {
     </button>
     <div className="hidden w-full md:block md:w-auto" id="navbar-default">
       <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-        <li>
+        
+      <li>
           <a href="#" className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</a>
         </li>
         <li>
+                  <Link
+                    href="/sell-an-item"
+                    className="block rounded bg-blue-700 py-2 pl-3 pr-4 text-white dark:text-white md:bg-transparent md:p-0 md:text-blue-700 md:dark:text-blue-500"
+                    aria-current="page"
+                  >
+                    Sell an Item
+                  </Link>
+                </li>
+        {!user.isSignedIn && (
+              <li>
                 <SignInButton>
                   <span className="cursor-pointer text-white hover:text-blue-500">
                     Sign In
                   </span>
                 </SignInButton>
               </li>
+            )}
         <li>
             <UserButton />
         </li>
