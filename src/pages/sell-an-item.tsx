1905dcf487c3436cd89c81ad604617d4f76e2d11
Sriram <sri761177@gmail.com>
2023-11-01 11:20:43 +0530

    Add textarea and button inputs for SellAnItem

--- src/pages/sell-an-item.tsx
+++ src/pages/sell-an-item.tsx
@@ -29,13 +29,58 @@ export default function SellAnItem() {
       <main className="bg-gray-900 flex min-h-screen flex-col items-center justify-center bg-gray-800">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
             <h1>Sell An Item</h1>
-     <form onSubmit={handleSubmit(onSubmit)}>
-        
-      <input {...register("name", { required: true})} />
-       <input {...register("description", { required: true })} />
-       <input type="number" step="0.01" {...register("price", { required: true })} />
-            <input type="submit" />
-    </form>
+     <form className="flex flex-col gap-4" 
+     onSubmit={handleSubmit(onSubmit)}>
+     <div>
+              <label
+                htmlFor="name"
+                className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Item Name
+              </label>
+              <input
+                id="name"
+                className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+                {...register("name", { required: true })}
+              />
+            </div>
+
+            <div>
+              <label
+                htmlFor="description"
+                className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Description
+              </label>
+              <textarea
+                id="description"
+                className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+                {...register("description", { required: true })}
+              />
+            </div>
+
+            <div>
+              <label
+                htmlFor="price"
+                className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Price
+              </label>
+              <input
+                id="price"
+                type="number"
+                step="0.01"
+                className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+                {...register("price", { required: true })}
+              />
+            </div>
+            <button
+              type="submit"
+              className="mb-2 mr-2 rounded-lg bg-blue-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Create
+            </button>
+            </form>
         </div>
       </main>
     </>
